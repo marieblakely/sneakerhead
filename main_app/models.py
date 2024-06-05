79616b77bc166afe8b3247d8eadda2c3c760326a
Marie Blakely <marieblakely@myyahoo.com>
2024-06-05 13:02:26 -0400

    create sneaker model and add a _str_ method

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Sneaker(models.Model):
+  style = models.CharField(max_length=100)
+  number = models.CharField(max_length=100)
+  description = models.TextField(max_length=250)
+
+
+  def __str__(self):
+    return self.style
