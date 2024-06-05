8081927b3af2e9350f6b9d6e29a0acd9793f5b6d
Marie Blakely <marieblakely@myyahoo.com>
2024-06-05 13:52:19 -0400

    add the update and delete routes in urls.py

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
   path('sneakers/', views.sneaker_index, name='sneaker-index'),
   path('sneakers/<int:sneaker_id>/', views.sneaker_detail, name='sneaker-detail'),
   path('sneakers/create/', views.SneakerCreate.as_view(), name='sneaker-create'),
+  path('sneakers/<int:pk>/update/', views.SneakerUpdate.as_view(), name='sneaker-update'),
+  path('sneakers/<int:pk>/delete/', views.SneakerDelete.as_view(), name='sneaker-delete'),
 ]
