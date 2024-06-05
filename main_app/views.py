6264592f89c8f2c059fd634bc682ad2780e29558
Marie Blakely <marieblakely@myyahoo.com>
2024-06-05 15:16:10 -0400

    import loginview class, replace the current home view function, and add update the path

--- main_app/views.py
+++ main_app/views.py
@@ -1,15 +1,13 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Sneaker
+from django.contrib.auth.views import LoginView
 
 
 
 
 
 # Create your views here.
-def home(request):
-  return render(request, 'home.html')
-
 def about(request):
   return render(request, 'about.html')
 
@@ -21,6 +19,9 @@ def sneaker_detail(request, sneaker_id):
   sneaker = Sneaker.objects.get(id=sneaker_id)
   return render(request, 'sneakers/detail.html', { 'sneaker': sneaker })
 
+class Home(LoginView):
+  template_name = 'home.html'
+  
 class SneakerCreate(CreateView):
   model = Sneaker
   fields = '__all__'
