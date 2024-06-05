809c803f9bdb43c3a6eb69e1843d7d0c886ab233
Marie Blakely <marieblakely@myyahoo.com>
2024-06-05 13:16:31 -0400

    update views.py to use sneaker model and import sneaker model

--- main_app/views.py
+++ main_app/views.py
@@ -1,20 +1,9 @@
 from django.shortcuts import render
+from .models import Sneaker
 
 
 
-class Sneaker:  
-  def __init__(self, style, number, description):
-    self.style = style
-    self.number = number
-    self.description = description
-  
 
-sneakers = [
-  Sneaker('Bred', '11', 'Brand new with original packaging.'),
-  Sneaker('Concord', '11', 'Brand new with original packaging.'),
-  Sneaker('Red Cement', '4', 'Like new. Barely worn with original packaging.'),
-  Sneaker('Chicago', '1', 'Worn. Does not have original packaging.')
-]
 
 # Create your views here.
 def home(request):
@@ -24,4 +13,5 @@ def about(request):
   return render(request, 'about.html')
 
 def sneaker_index(request):
+  sneakers = Sneaker.objects.all()
   return render(request, 'sneakers/index.html', { 'sneakers': sneakers })
