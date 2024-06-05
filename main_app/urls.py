4bebc38b0bbc45556278252f66e53509e8e10009
Marie Blakely <marieblakely@myyahoo.com>
2024-06-05 12:36:54 -0400

    create and the route and view function for index

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
+  path('sneakers/', views.sneaker_index, name='sneaker-index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+class Sneaker:  
+  def __init__(self, style, number, description):
+    self.name = style
+    self.breed = number
+    self.description = description
+  
+
+sneakers = [
+  Sneaker('Bred', '11', 'Brand new with original packaging.'),
+  Sneaker('Concord', '11', 'Brand new with original packaging.'),
+  Sneaker('Red Cement', '4', 'Like new. Barely worn with original packaging.'),
+  Sneaker('Chicago', '1', 'Worn. Does not have original packaging.')
+]
+
 # Create your views here.
 def home(request):
   return HttpResponse('<h1>Hello Sneakerhead👟</h1>')
