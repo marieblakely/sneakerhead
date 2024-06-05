4e90c7cd33e74f58274919c3d7af5646ae829de5
Marie Blakely <marieblakely@myyahoo.com>
2024-06-05 16:47:23 -0400

    update sign up form

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Sneaker
 from django.contrib.auth.views import LoginView
