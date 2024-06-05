a355274df039bc8e984ca67cd1d9eccdc92c81e1
Marie Blakely <marieblakely@myyahoo.com>
2024-06-05 13:55:53 -0400

    update details UI to include edit and delete links, add updateview and deleteview to views.py

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Sneaker
 
 
@@ -25,3 +25,11 @@ class SneakerCreate(CreateView):
   model = Sneaker
   fields = '__all__'
   success_url = '/sneakers/'
+
+class SneakerUpdate(UpdateView):
+  model = Sneaker
+  fields = ['style', 'number', 'description']
+
+class SneakerDelete(DeleteView):
+  model = Sneaker
+  success_url = '/sneakers/'  
